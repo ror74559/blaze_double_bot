0f86c0f94fcb041e2afe8ca3804ca2b917821936
Rafael Ribeiro <61093652+ror74559@users.noreply.github.com>
2024-05-22 14:30:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+with open('README.md, 'r') as f:
+          readme =f.read()
 
 setup(
     name='blaze_double_bot',
     version='0.1.0',
+    author='Rafael de Oliveira Ribeiro',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='ror74559@gmail.com',
+    description='This Python script automates the betting process on the Blaze Double',
     packages=find_packages(),
     install_requires=[
         'undetected_chromedriver',
